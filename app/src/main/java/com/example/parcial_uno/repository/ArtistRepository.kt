52802ef8e8445cba80fe25231e333a0aa0296d7e
Moiezequiel <00038221@uca.edu.sv>
2023-05-22 14:53:01 -0600

    3.Capa repository

---
+++ app/src/main/java/com/example/parcial_uno/repository/ArtistRepository.kt
@@ -0,0 +1,10 @@
+package com.example.parcial_uno.respository
+
+import android.provider.MediaStore.Audio.Artists
+import com.example.parcial_uno.data.model.ArtistModel
+
+class ArtistRepository (private  val artists: Artists: MutableList<ArtisteModel>){
+    fun getMovies () = artist
+
+    fun setMovies (movie: ArtistModel) = Artist.add(artist)
+}
